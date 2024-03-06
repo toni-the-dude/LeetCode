81c7ff3332c6c021c29aa5d18db64c99686de2c5
Ștefan Emil - Antoniu <76394495+toni-the-dude@users.noreply.github.com>
2024-03-06 17:31:26 +0200

    Create Product of Array Except Self.py

---
+++ Product of Array Except Self.py
@@ -0,0 +1,21 @@
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        answer = []
+        zeros = 0
+        product_of_all = 1
+
+        for number in nums:
+            if number == 0: zeros += 1
+            else: product_of_all *= number
+
+        index = 0
+        while index < len(nums):
+            if nums[index] == 0 and zeros == 1:
+                answer.append(product_of_all)
+            elif zeros >= 1:
+                answer.append(0)
+            else:
+                answer.append(product_of_all // nums[index])
+            index += 1
+
+        return answer
