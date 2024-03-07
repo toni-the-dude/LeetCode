7eb76c4f37f56a08c06e0302ec833531a396ecd1
Ștefan Emil - Antoniu <76394495+toni-the-dude@users.noreply.github.com>
2024-03-07 09:53:19 -0700

    Time: 102 ms (86.33%), Space: 17.2 MB (73.47%) - LeetHub

--- 1456-maximum-number-of-vowels-in-a-substring-of-given-length/1456-maximum-number-of-vowels-in-a-substring-of-given-length.py
+++ 1456-maximum-number-of-vowels-in-a-substring-of-given-length/1456-maximum-number-of-vowels-in-a-substring-of-given-length.py
@@ -10,28 +10,23 @@ class Solution:
             if s[head] in "aeiou":
                 current_vow += 1
             head += 1
+            
         head -= 1
         max_vow = current_vow
-        # print("Current number of vowels:{0}".format(current_vow))
         
         while head < len(s):
+            
             if s[tail] in "aeiou":
                 current_vow -= 1
-                # print("Lost the vowel {0}".format(s[tail]))
+                
             tail += 1
             head += 1
-            # print(tail)
-            # print(head)
+            
             if head != len(s):
-                # print("It's not the end")
                 if s[head] in "aeiou":
-                    # print("Gained another")
                     current_vow += 1
-                    # print("Gained the vowel {0}".format(s[head]))
                 
             if current_vow > max_vow:
                 max_vow = current_vow
-            # print("Current number of vowels:{0}".format(current_vow))
-
         
         return max_vow
