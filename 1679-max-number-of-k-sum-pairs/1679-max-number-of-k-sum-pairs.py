85cfb1ca614802b0906737c2eeae9bfd0a7b005a
Ștefan Emil - Antoniu <76394495+toni-the-dude@users.noreply.github.com>
2024-03-07 09:22:40 -0700

    Time: 502 ms (43.12%), Space: 29.6 MB (46.16%) - LeetHub

--- 1679-max-number-of-k-sum-pairs/1679-max-number-of-k-sum-pairs.py
+++ 1679-max-number-of-k-sum-pairs/1679-max-number-of-k-sum-pairs.py
@@ -12,23 +12,24 @@ class Solution:
         print(dictionary)
         
         for key, value in dictionary.items():
-            print(key, value)
             if (k - key) in dictionary.keys():
                 while dictionary[key] != 0:
-                    print("Found {0} paired with {1}".format(key, k - key))
+                    
                     if key == (k - key) and dictionary[key] != 1:
                         dictionary[key] -= 2
                         answer += 1
                         continue
+                        
                     if key == (k - key) and dictionary[key] == 1:
                         dictionary[key] -= 1
                         continue
+                        
                     if dictionary[key] > 0 and dictionary[k - key] > 0:
                         answer += 1
-                        print("Current answer:{0}".format(answer))
                         dictionary[key] -= 1
                         dictionary[k - key] -= 1
+                        
                     elif dictionary[key] > 0 and dictionary[k - key] == 0:
                         dictionary[key] = 0
-        print(dictionary)
+                        
         return answer
