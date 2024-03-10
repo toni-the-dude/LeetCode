ca5685b93620af6aad91a6ea94a1f3b962862a3d
Ștefan Emil - Antoniu <76394495+toni-the-dude@users.noreply.github.com>
2024-03-10 10:24:16 -0600

    Time: 187 ms (5.06%), Space: 18 MB (65.33%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.py
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.py
@@ -2,9 +2,8 @@ class Solution:
     def removeDuplicates(self, nums: List[int]) -> int:
         k = 0
         i = 0
+        
         while i < len(nums):
-            # print(nums[i])
-            # print(nums)
             if nums[i] == '_': 
                 break
             try:
@@ -18,6 +17,5 @@ class Solution:
                 k += 1
                 break
             i += 1
-        # print(nums)
-        # print(k)
+            
         return k
