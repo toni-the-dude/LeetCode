9f0e1f7040da19aa2f319aa92028a95baa0d864c
Ștefan Emil - Antoniu <76394495+toni-the-dude@users.noreply.github.com>
2024-03-13 03:01:34 -0600

    Time: 383 ms (76.41%), Space: 21.6 MB (40.94%) - LeetHub

--- 2352-equal-row-and-column-pairs/2352-equal-row-and-column-pairs.py
+++ 2352-equal-row-and-column-pairs/2352-equal-row-and-column-pairs.py
@@ -6,7 +6,7 @@ class Solution:
         
         for i in range(0, n, 1):
             row = tuple(grid[i:][0])
-            print(row)
+            
             if row not in fr:
                 fr[row] = 1
             else:
@@ -14,15 +14,12 @@ class Solution:
         
         for i in range(0, n, 1):
             temp = []
+            
             for j in range(0, n, 1):
-                # print(grid[j][i])
                 temp.append(grid[j][i])
-                # print(grid[j][i])
-                # print(temp)
                 
-            # print(temp)
             temp = tuple(temp)
-            print(temp)
+            
             if temp in fr:
                 print("match")
                 pairs += fr[temp]
