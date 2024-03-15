0da1f8e47a4ab042cdc3788181a9a17873a45155
Ștefan Emil - Antoniu <76394495+toni-the-dude@users.noreply.github.com>
2024-03-15 11:37:35 -0600

    Time: 110 ms (23.64%), Space: 17.2 MB (21.28%) - LeetHub

--- 0649-dota2-senate/0649-dota2-senate.py
+++ 0649-dota2-senate/0649-dota2-senate.py
@@ -6,25 +6,13 @@ class Solution:
         l = len(senate)
         
         for i in range(0, l, 1):
-            # print("Radiants:{0}".format(rads))
-            # print("Dires:{0}".format(dires))
-            # print(i)
             if senate[i] == "R":
-                # print("HERE")
                 rads.append(i)
-                # print(rads)
             else:
-                # print("THERE")
                 dires.append(i)
-                # print(dires)
-            # print(rads)
-            # print(dires)
               
             
         while rads != [] and dires != []:
-            # print("Radiants:{0}".format(rads))
-            # print("Dires:{0}".format(dires))
-            # print(i, j)
             if rads[0] < dires[0]:
                 dires.pop(0) # Lost
                 rads.append(rads.pop(0) + l) # Winner enter queue again
