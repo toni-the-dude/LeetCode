b6222519b2cea962c2906f749ab074746cafaafd
Ștefan Emil - Antoniu <76394495+toni-the-dude@users.noreply.github.com>
2025-07-04 20:32:55 +0300

    Time: 0 ms (100%), Space: 17.7 MB (49.78%) - LeetHub

---
+++ 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.py
@@ -0,0 +1,8 @@
+class Solution:
+    def checkIfPangram(self, sentence: str) -> bool:
+        
+        for i in range(97, 123, 1):
+
+            if chr(i) not in sentence: return False
+
+        return True
